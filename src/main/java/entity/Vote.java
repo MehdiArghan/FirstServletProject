f2c2cd0810f92085f1f1e0deb0e8cf21bab42855
mehdi <mehdiarghan@gmail.com.git/>
2024-02-11 22:53:21 +0330

    added Vote class

--- src/main/java/entity/Vote.java
+++ src/main/java/entity/Vote.java
@@ -2,7 +2,16 @@ package entity;
 
 import base.entity.BaseEntity;
 import jakarta.persistence.Entity;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
 @Entity
 public class Vote extends BaseEntity<Long> {
+    String vote;
 }
