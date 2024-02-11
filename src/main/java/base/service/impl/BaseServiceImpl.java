4b888af8d4560990222201e9b5df15aca000d020
mehdi <mehdiarghan@gmail.com.git/>
2024-02-11 22:37:18 +0330

    added BaseServiceImpl class

---
+++ src/main/java/base/service/impl/BaseServiceImpl.java
@@ -0,0 +1,65 @@
+package base.service.impl;
+
+import base.entity.BaseEntity;
+import base.repository.BaseRepository;
+import base.service.BaseService;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.TransactionalException;
+import lombok.AllArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+public class BaseServiceImpl<ID extends Serializable, Entity extends BaseEntity<ID>
+        , Repository extends BaseRepository<ID, Entity>> implements BaseService<ID, Entity> {
+    protected EntityManager entityManager;
+    protected Repository repository;
+
+    @Override
+    public void save(Entity entity) {
+        try {
+            entityManager.getTransaction().begin();
+            repository.save(entity);
+            entityManager.getTransaction().commit();
+        } catch (TransactionalException e) {
+            entityManager.getTransaction().rollback();
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void update(Entity entity) {
+        try {
+            entityManager.getTransaction().begin();
+            repository.update(entity);
+            entityManager.getTransaction().commit();
+        } catch (TransactionalException e) {
+            entityManager.getTransaction().rollback();
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void remove(Entity entity) {
+        try {
+            entityManager.getTransaction().begin();
+            repository.remove(entity);
+            entityManager.getTransaction().commit();
+        } catch (TransactionalException e) {
+            entityManager.getTransaction().rollback();
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Optional<Entity> findById(ID id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public List<Entity> findAll() {
+        return repository.findAll();
+    }
+}
