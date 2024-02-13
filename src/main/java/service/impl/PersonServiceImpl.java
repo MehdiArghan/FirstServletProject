4023db0598cb41fee78ee4ebb5bd2ade2cc5f7ca
mehdi <mehdiarghan@gmail.com.git/>
2024-02-13 21:31:25 +0330

    added PersonServiceImpl class

---
+++ src/main/java/service/impl/PersonServiceImpl.java
@@ -0,0 +1,13 @@
+package service.impl;
+
+import base.service.impl.BaseServiceImpl;
+import entity.Person;
+import jakarta.persistence.EntityManager;
+import repository.PersonRepository;
+import service.PersonService;
+
+public class PersonServiceImpl extends BaseServiceImpl<Long, Person, PersonRepository> implements PersonService {
+    public PersonServiceImpl(EntityManager entityManager, PersonRepository repository) {
+        super(entityManager, repository);
+    }
+}
